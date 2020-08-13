4f0c59418c9c021899ee74a103aa452935d3dffb
Mauricio Vieira <mauriciorenanvs@gmail.com>
2020-08-13 20:06:44 -0300

    fix: adjusted validate messages and interfaces

--- src/components/MessageWrapper/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const MessageWrapper = styled.div`
-  padding: 3px 0;
-  position: relative;
-`;
-
-export default MessageWrapper;
--- src/context/Auth/actions.ts
+++ src/context/Auth/actions.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from 'react';
-import { ILogin, IAuth } from '../../contracts/Auth/Action/login.interface';
+import { ILogin, IAuth } from '../../interfaces/Auth/Action/login.interface';
 
 export const login = (dispatch: Dispatch<ILogin>, payload: IAuth) => {
   dispatch({
