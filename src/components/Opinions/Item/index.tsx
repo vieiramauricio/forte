0714d113e9c43ce2060db60bbf8936bc00efb819
Mauricio Vieira <mauriciorenanvs@gmail.com>
2020-08-15 17:28:18 -0300

    feat: finished opinion list style

--- src/components/Opinions/Item/index.tsx
+++ src/components/Opinions/Item/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import IOpinion from '../../../interfaces/Opinion/opinion.interface';
-import { Box, TextField } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { ArrowUpward } from '@material-ui/icons';
 
-// import { Container } from './styles';
+import { Container, Upvote, Title, Header } from './style';
+
+const ReactMarkdown = require('react-markdown');
+
 
 const Item: React.FC <IOpinion>= ({id, title, content, upvotes_count}) => {
   return (
-    <Box>
-      <Box>{upvotes_count}</Box>
+    <Container>
+      <Upvote>
+        <ArrowUpward fontSize="large"/>
+        <span>{upvotes_count}</span>
+      </Upvote>
       <Box>
+        <Header>
+          <Title>
+            {title}
+          </Title>
+        </Header>
         <Box>
-          {title}
-        </Box>
-        <Box>
-          {content}
+          <ReactMarkdown source={content}/>
         </Box>
       </Box>
-    </Box>
+    </Container>
   );
 }
 
