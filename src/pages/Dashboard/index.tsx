cf1f432bbdab22738b0f019b525e798cb5412f1d
Mauricio Vieira <mauriciorenanvs@gmail.com>
2020-08-15 18:04:18 -0300

    feat: filter by title

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,28 +2,42 @@ import React, { useState, useEffect } from 'react';
 import Inside from '../../layouts/Inside';
 import { useAuth } from '../../context/Auth';
 import { Subtitle } from '../../components/Title';
-import { Box, TextField } from '@material-ui/core';
+import { Box, TextField, InputAdornment } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
 import ButtonWrapper from '../../components/Button';
 import List from '../../components/Opinions/List';
 import api from '../../services/api';
+import IOpinion from '../../interfaces/Opinion/opinion.interface';
 
 
 const Dashboard: React.FC = () => {
   const { state } = useAuth();
-  const { token } = state;
+  // const { token } = state;
   const [opinions, setOpinions] = useState([]);
+  const [view, setView] = useState([]);
 
-  const getOpinions =  async () =>{
+  const getOpinions = async () =>{
     try{
       const res = await api.get('/opinions');
       setOpinions(res.data.opinions);
+      setView(res.data.opinions);
     }catch(e){
       console.log(e)
     }
   }
+
+  const handleFilter = (title: string) => {
+    const opinionsFiltered = opinions.filter((item: IOpinion) => {
+      return(
+        item.title.toLowerCase().includes(title)
+      )
+    });
+    setView(opinionsFiltered);
+  };
+
   useEffect(() => {
     getOpinions();
-  }, [])
+  }, []);
 
   return (
     <Inside>
@@ -39,20 +53,24 @@ const Dashboard: React.FC = () => {
       </Box>
 
       <Box display="flex">
-        <Box mr={2}>
+      <Box mr={1}>
           <TextField
-            label="Nome"
+            label="Título"
             variant="outlined"
-            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            onChange={(e) => handleFilter(e.target.value)}
           />
         </Box>
-        <ButtonWrapper color="primary" size="100px">
-          Buscar
-        </ButtonWrapper>
       </Box>
 
       <Box>
-        <List opinionList={opinions}/>
+        <List opinionList={view}/>
       </Box>
 
     </Inside>
